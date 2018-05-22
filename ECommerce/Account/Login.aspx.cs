eedd60cb285bffa23744a98454fc47d7542a64f2
GGolov <gleb.golov@gmail.com>
2018-05-22 23:22:10 +0200

    Various changes

--- ECommerce/Account/Login.aspx.cs
+++ ECommerce/Account/Login.aspx.cs
@@ -12,7 +12,7 @@ public partial class Account_Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
 
     protected void btnLogin_Click(object sender, EventArgs e)
@@ -25,10 +25,30 @@ public partial class Account_Login : System.Web.UI.Page
         command.Parameters.AddWithValue("@pEmail", email);
 
         connection.Open();
-        
+
+        SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                string passwordHash = reader["PasswordHash"].ToString();
+                bool isEqual = Verify(password, passwordHash);
+
+                if (isEqual)
+                {
+                    Session["FullName"] = reader["Name"] + " " + reader["Surname"];
+                    Session["Email"] = email;
+
+                    Response.Redirect("/");
+                }
+                else
+                {
+                    Response.Redirect("/Account/Login");
+                }
+            }
+        }
 
         connection.Close();
-
-        Response.Redirect("/");
     }
 }
---
+++ ECommerce/Account/Profile.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Profile.aspx.cs" Inherits="Account_Profile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphHead" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphMain" Runat="Server">
+  <div class="container">
+    <div class="card">
+      <div class="card-content">
+        <span class="card-title"><%=Session["FullName"]%></span>
+        <p><%=Session["Email"]%></p>
+      </div>
+    </div>
+  </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cphScripts" Runat="Server">
+</asp:Content>
+
